50278c29e59130409cc2c9f265011378b7a5bbd9
Shannon Kearney <shannon.kearney@colorado.edu>
2020-04-12 11:19:52 -0600

    final

--- README.md
+++ README.md
@@ -8,9 +8,11 @@ Homework 3
 [x] Function to prompt user for password options
 //function getPasswordOptions()
 [x] Variable to store length of password from user input
-[x] Conditional statement to check if password length is a number. Prompts end if this evaluates false
+[ ] Conditional statement to check if password length is a number. Prompts end if this evaluates false
+
 [x] Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
 [x] Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+
 [x] Variable to store boolean regarding the inclusion of special characters
 [x] Variable to store boolean regarding the inclusion of numeric characters
 [x] Variable to store boolean regarding the inclusion of lowercase characters
@@ -24,19 +26,32 @@ Homework 3
 [x] Function for getting a random element from an array
 [x] Function to generate password with user input
 [x] Array to store types of characters to include in password
+
 [ ] Array to contain one of each type of chosen character to ensure each will be used
+
 [ ] Conditional statement that adds array of special characters into array of possible characters based on user input
+
 [ ] Push new random special character to guaranteedCharacters
+
 [ ] Conditional statement that adds array of numeric characters into array of possible characters based on user input
+
 [ ] Push new random special character to guaranteedCharacters
+
 [ ] Conditional statement that adds array of lowercase characters into array of possible characters based on user input
+
 [ ] Push new random lower-cased character to guaranteedCharacters
+
 [ ] Conditional statement that adds array of uppercase characters into array of possible characters based on user input
+
 [ ] Push new random upper-cased character to guaranteedCharacters
+
 [ ] For loop to iterate over the password length from the options object, selecting random indices from the array of possible characters and concatenating those characters into the result variable
+
 [ ] Mix in at least one of each guaranteed character in the result
-[ ] Transform the result into a string and pass into writePassword
+[x] Transform the result into a string and pass into writePassword
 //return result.join('');
+
+
 [ ] Get references to the #generate element
 [ ] Function to Write password to the #password input
 [ ] Add event listener to generate button
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //arrays:
 var specialChar = ["+", "-", "&", "|", "!", "(", ")",  "{", "}", "[", "]", "^", "~", "*", "?", ":", "/"];
 
-var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+var numbers = [1,2,3,4,5,6,7,8,9];
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -13,88 +13,76 @@ var otherChars = numbers.concat(lowerCase,upperCase);
 var allChars = specialChar.concat(numbers,lowerCase,upperCase);
 
 //console.log(otherChars);
-console.log(allChars);
-
-//function createPassword(charLength, capitalLetters = false, specialLetters = false, lowercaseLetters=false ) {
-    //console.log(charLength, lowercaseLetters, capitalLetters, specialLetters)
-    //return password
-//}
-//stores user input length
-var length = prompt("Enter a number bewteen 8 and 128 for the length of your generated password: ");
-var lengthNum = Number(length);
-console.log(typeof lengthNum);
-//how do we check this user input is a number? 
-//if statement to check length requirements. Could this be in a function
-
-if (lengthNum < 8 || lengthNum > 128 ){
+//console.log(allChars);
 
 
-    var length= prompt("Enter a number bewteen 8 and 128 for the length of your generated password:");
-    var lengthNum = Number(length);
+function generatePassword (){
 
-}else{
+    var length = prompt("Enter a number bewteen 8 and 128 for the length of your generated password: "); //user input for length of password
+    var lengthNum = Number(length); //converting the user input length from a string to a number 
+    //console.log(lengthNum);
 
-    
-};
+    if (lengthNum < 8 || lengthNum > 128 ){ //conditional statement to check length requirements
 
-//booelan special characters/lowercase/uppercase
-var incCharSpecial = confirm("Include special characters?");
-var incCharOthers= confirm("Include Numbers, lower case and upper case letters?");
-//var incLowerChar = confirm("Include lower case characters?");
-//var incUpperChar = confirm("Include upper case characters?");
 
-function generatePassword () {
-var password = [];
- //createPassword(110, capitalLetters=true)   
-    //function createPassword(charLength, capitalLetters = false, specialLetters = false, lowercaseLetters=false ) {
-    //console.log(charLength, charSpecial, charNum, lowerChar, upperChar)
-    //return password
-    if(incCharSpecial && incCharOthers === false){
-        for (var i = 0; i < lengthNum; i++) {
-          var test = specialChar[Math.floor(Math.random() * specialChar.length)]; //creating a new array with random elements from specialChar with length = userinput from prompt. 
-          password.push(test); //new array
+        var length= prompt("Please enter a number greater than 8 and less than 128:");
+        var lengthNum = Number(length);
+
+    }else if (isNaN(lengthNum) === true){ //conditional statement to check if length is a number
+
+        var length= prompt("Enter a NUMBER bewteen 8 and 128 for the length of your generated password:");
+        var lengthNum = Number(length);
+
+        
+    };
+
+    //booelan special characters/lowercase/uppercase
+    var incCharSpecial = confirm("Include special characters?");
+    var incCharOthers= confirm("Include numbers, lower case and upper case letters?");
+
+    var password = [];
+
+        if(incCharSpecial && incCharOthers === false){ // conditional statements generating password based on true/false user input character requirements
+            for (var i = 0; i < lengthNum; i++) { //iterates over user input length 
+            var test = specialChar[Math.floor(Math.random() * specialChar.length)]; //generating random elements from chosen characcter array, iterating over user input length
+            password.push(test); //i random elements pushed to a new array
+            }
+            return password.join("");//
+            //console.log(password.join("")); //string
+        } else if (incCharSpecial && incCharOthers){
+            for (var i = 0; i < lengthNum; i++) {
+                var test = allChars[Math.floor(Math.random() * allChars.length)]; 
+                password.push(test); 
+            }
+            return password.join("");
+            //console.log(password.join(""));
+        }else if (incCharSpecial === false && incCharOthers){
+            for (var i = 0; i < lengthNum; i++) {
+                var test = otherChars[Math.floor(Math.random() * otherChars.length)]; 
+                password.push(test); 
+            }
+            return password.join("");
+            //console.log(password.join(""));
+        }else { //if user input is false for both character checkers
+
+            return("No characters selected.");
         }
-        return password.join("");
-        //console.log(password.join("")); //string
-        //return password.join("") 
-    } else if (incCharSpecial && incCharOthers){
-        for (var i = 0; i < lengthNum; i++) {
-            var test = allChars[Math.floor(Math.random() * allChars.length)]; //creating a new array with random elements from specialChar with length = userinput from prompt. 
-            password.push(test); //new array
-          }
-          return password.join("");
-          //console.log(password);
-    }else if (incCharSpecial === false && incCharOthers){
-        for (var i = 0; i < lengthNum; i++) {
-            var test = otherChars[Math.floor(Math.random() * otherChars.length)]; //creating a new array with random elements from specialChar with length = userinput from prompt. 
-            password.push(test); //new array
-          }
-          return password.join("");
-          //console.log(password);
-    }else {
 
-        return;
-    }
-    //return password.join("");
-};
-      
 
-generatePassword();
+    };
+                        
 
-            
-            
-//GIVEN CODE 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+    var generateBtn = document.querySelector("#generate"); //target generate password button
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    // Write password to the #password input
+    function writePassword() {
+    var password = generatePassword(); 
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password; 
 
 };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
